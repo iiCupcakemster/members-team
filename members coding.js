4533ad6bf437e4564adbaf0450ba054ca3a120ed
iiCupcakemster <101678628+iiCupcakemster@users.noreply.github.com>
2022-09-10 11:28:01 -0700

    client.on('message', async (message) => {   if (message.author.bot) return;   const args = message.content.slice(prefix.length).split(/ +/);   const command = args.shift().toLowerCase();
      const roleName = args.join(' ');
      const roles = await message.guild.roles.fetch();
      const role = roles.cache.find(
        (r) => r.name.toLowerCase() === roleName.toLowerCase(),
      );
      if (!role)
        return message.channel.send(
          `**Error:** ${roleName} is not a valid role on this server`,
        );
      const members = await message.guild.members.fetch();
      const usersWithRole = members.filter((member) =>
        member.roles.cache.has(role.id),
      );
    
      const embed = new Discord.MessageEmbed()
        .setTitle(`Users with ${roleName} role`)
        .setDescription(`${usersWithRole.array().join('\n')}`)
        .setColor('RANDOM')
        .setFooter(
          `${message.author.username}`,
          message.author.avatarURL({ dynamic: true }),
        );
    
      return message.channel.send(embed);
    });

--- members coding.js
+++ members coding.js
@@ -1,19 +1,30 @@
-if(message.content.startsWith("//inrole")){
-    let roleName = message.content.split(" ").slice(1).join(" ");
-
-    //Filtering the guild members only keeping those with the role
-    //Then mapping the filtered array to their usernames
-    let membersWithRole = message.guild.members.filter(member => { 
-        return member.roles.find("name", roleName);
-    }).map(member => {
-        return member.user.username;
-    })
-
-    let embed = new discord.RichEmbed({
-        "title": `Users with the ${roleName} role`,
-        "description": membersWithRole.join("\n"),
-        "color": 0xFFFF
-    });
-
-    return message.channel.send({embed});
-}
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+  
+    const roleName = args.join(' ');
+    const roles = await message.guild.roles.fetch();
+    const role = roles.cache.find(
+      (r) => r.name.toLowerCase() === roleName.toLowerCase(),
+    );
+    if (!role)
+      return message.channel.send(
+        `**Error:** ${roleName} is not a valid role on this server`,
+      );
+    const members = await message.guild.members.fetch();
+    const usersWithRole = members.filter((member) =>
+      member.roles.cache.has(role.id),
+    );
+  
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Users with ${roleName} role`)
+      .setDescription(`${usersWithRole.array().join('\n')}`)
+      .setColor('RANDOM')
+      .setFooter(
+        `${message.author.username}`,
+        message.author.avatarURL({ dynamic: true }),
+      );
+  
+    return message.channel.send(embed);
+  });
